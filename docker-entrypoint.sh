adc79d4160c149740d91cf7f6500c7ec1a788a3b
nabad600 <nabad600@gmail.com>
2021-04-02 11:33:24 +0530

    Update

---
+++ docker-entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+cd /var/www
+export COMPOSER_PROCESS_TIMEOUT=2600
+if [ -d "bin" ]; then 
+echo "Git already exists"
+composer install
+else
+git clone https://github.com/nabad600/sylius.git .
+echo "Download completed..."
+fi
+
+cp /sbin/app /var/www/.env
+cd /var/www
+
+bin/console sylius:install
+# until bin/console doctrine:query:sql "select 1" >/dev/null 2>&1; do
+# 	    (>&2 echo "Waiting for MySQL to be ready...")
+# 		sleep 1
+# done
+
+bin/console doctrine:migrations:migrate --no-interaction
+
+exec "$@"
